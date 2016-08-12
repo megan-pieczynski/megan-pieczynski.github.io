b16e3e147533311955a6b7d7ab2a59e7c8a38dd6
Megan <mapieczynski@gmail.com>
2016-08-12 08:48:28 -0700

    style file added

---
+++ script.js
@@ -0,0 +1,24 @@
+$('#a').hover(function() {
+        $('#shroomhaze').toggleClass('on');
+});
+
+$('#b').hover(function() {
+        $('#influenza').toggleClass('on');
+});
+
+$('#c').hover(function() {
+        $('#linkedin').toggleClass('on');
+});
+
+$('#d').hover(function() {
+        $('#pinot').toggleClass('on');
+});
+
+$('#e').hover(function() {
+        $('#pbliss').toggleClass('on');
+});
+
+$('#f').hover(function() {
+        $('#pbliss').toggleClass('on');
+});
+
